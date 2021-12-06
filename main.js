4e9d8584115ba2f97f62666d1d7ede9de0ecf6a3
Tarun Naik <78128450+TarunNaik-beep@users.noreply.github.com>
2021-12-06 16:57:04 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,63 @@
+video = "";
+myStatus = "";
+objects = [];
+
+function setup() {
+    canvas = createCanvas(480,380);
+    canvas.center();
+
+    video = createCapture(VIDEO);
+    video.size(480,300);
+    video.hide();
+}
+
+function draw() {
+    image(video,0,0.480,380);
+    if(myStatus!="") {
+        object_detector.detect(video,gotResults);
+        for(i=0; i<objects.length;i++) {
+            document.getElementById("status").innerHTML = "status: Objects Detected";
+            fill("#FF0000");
+            percent = floor(objects[i].confidence*100);
+            text(objects[i].label + " " + percent + "%",objects[i].x+15, objects[i].y+15);
+            noFill();
+            stroke("#FF0000");
+            rect(objects[i].x,objects[i].y,objects[i].width,objects[i].height);
+            if(objects[i].label==object_name) {
+                video.stop();
+                object_detector.detect(gotResults);
+                document.getElementById("status").innerHTML = object_name + "found";
+                synth = window.speechSynthesis;
+                utterThis = new SpeechSynthesisUtterance(object_name + "found");
+                synth.speak(utterThis);
+            }
+            else {
+                document.getElementById("status").innerHTML = object_name + " not found"; 
+            }
+        
+        }
+    }
+}
+
+function preload() {
+}
+
+function start() {
+    object_detector = ml5.objectDetector('cocossd', modelLoaded);
+    document.getElementById("status").innerHTML = "status: detecting objects";
+    object_name = document.getElementById("object_name").value;
+}
+
+function modelLoaded() {
+    console.log("modelLoaded");
+    myStatus = true;
+
+function gotResults(error, results) {
+    if(error) {
+        console.error(error);
+    }
+    else {
+        console.log(results);
+        objects = results;
+    }
+}}
